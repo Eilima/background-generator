fd04f6afabc5e1ba1ea58b73186c9f66f297b779
ExciteTaco <ericklima34@gmail.com>
2020-01-05 21:14:16 -0600

    Fixed problem with random button and add some functionality to input bttn

--- script.js
+++ script.js
@@ -5,18 +5,23 @@ var body = document.getElementById("gradient");
 var rndBttn = document.getElementById("rnd");
 var hex = document.getElementById("hex");
 
-//
 
-function inputValue(test1, test2){
-    color1.setAttribute("value", hexVal1);
-    color2.setAttribute("value", hexVal2);
-}
+
+//  function inputValue(hexVal1, hexVal2){
+//      color1.setAttribute("value", hexVal1);
+//      color2.setAttribute("value", hexVal2);
+//  }
 
 
 function gradient(){
+    let colorVal = [];
+    colorVal.push(color1.value);
+    colorVal.push(color2.value);
     body.style.background = "linear-gradient(to right, " + color1.value 	+ ", " + color2.value + ")";
+    console.log(body.style.backgroundColor);
+    // rbg.textContent = body.style.backgroundColor + ";";
     rbg.textContent = body.style.backgroundColor + ";";
-    hex.textContent = prevColor;
+    hex.textContent = colorVal;
 }
 
 // Adds functionality to the "random" button on the page. Randomly generates hex values on every click.
@@ -28,15 +33,13 @@ function randomButton(){
     hexVal1 = '#' + Math.random().toString(16).slice(2, 8).toUpperCase();
     hexVal2 = '#' + Math.random().toString(16).slice(2, 8).toUpperCase();
     // Push generated hex values to the array created at the begining of the function (prevColor).
-    previous.push(hexVal1);
-    previous.push(hexVal2);
+    prevColor.push(hexVal1);
+    prevColor.push(hexVal2);
     // Changes the pages background color to the one previously generated.
     body.style.background = "linear-gradient(to right, " + hexVal1 + ", " + hexVal2 + ")"
     // Add the rgb and hex values to the respective tags in the index.html file.
     rbg.textContent = body.style.background + ";";
     hex.textContent = prevColor;
-   
-    
 }
 
 // Adding an event listener to the input buttons in the webpage that "listen" for a input and when a input is selected the function gradient is run.
