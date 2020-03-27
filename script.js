951cb4b1e6ff7b1659b77793eae2ee203841e43d
ExciteTaco <ericklima34@gmail.com>
2020-03-26 23:31:03 -0500

    changing title and updating all files with new formatting

--- script.js
+++ script.js
@@ -5,45 +5,54 @@ var body = document.getElementById("gradient");
 var rndBttn = document.getElementById("rnd");
 var hex = document.getElementById("hex");
 
-
-
 //  function inputValue(hexVal1, hexVal2){
 //      color1.setAttribute("value", hexVal1);
 //      color2.setAttribute("value", hexVal2);
 //  }
 
-
-function gradient(){
-    let colorVal = [];
-    colorVal.push(color1.value);
-    colorVal.push(color2.value);
-    body.style.background = "linear-gradient(to right, " + color1.value 	+ ", " + color2.value + ")";
-    console.log(body.style.backgroundColor);
-    // rbg.textContent = body.style.backgroundColor + ";";
-    rbg.textContent = body.style.backgroundColor + ";";
-    hex.textContent = colorVal;
+function gradient() {
+  let colorVal = [];
+  colorVal.push(color1.value);
+  colorVal.push(color2.value);
+  body.style.background =
+    "linear-gradient(to right, " + color1.value + ", " + color2.value + ")";
+  console.log(body.style.backgroundColor);
+  // rbg.textContent = body.style.backgroundColor + ";";
+  rbg.textContent = body.style.backgroundColor + ";";
+  hex.textContent = colorVal;
 }
 
 // Adds functionality to the "random" button on the page. Randomly generates hex values on every click.
 
-function randomButton(){
-    // Creates an array used to store the randomly generated hex values.
-    var prevColor = [];
-    // Generates and stores 2 hex values using Math.random().
-    hexVal1 = '#' + Math.random().toString(16).slice(2, 8).toUpperCase();
-    hexVal2 = '#' + Math.random().toString(16).slice(2, 8).toUpperCase();
-    // Push generated hex values to the array created at the begining of the function (prevColor).
-    prevColor.push(hexVal1);
-    prevColor.push(hexVal2);
-    // Changes the pages background color to the one previously generated.
-    body.style.background = "linear-gradient(to right, " + hexVal1 + ", " + hexVal2 + ")"
-    // Add the rgb and hex values to the respective tags in the index.html file.
-    rbg.textContent = body.style.background + ";";
-    hex.textContent = prevColor;
+function randomButton() {
+  // Creates an array used to store the randomly generated hex values.
+  var prevColor = [];
+  // Generates and stores 2 hex values using Math.random().
+  hexVal1 =
+    "#" +
+    Math.random()
+      .toString(16)
+      .slice(2, 8)
+      .toUpperCase();
+  hexVal2 =
+    "#" +
+    Math.random()
+      .toString(16)
+      .slice(2, 8)
+      .toUpperCase();
+  // Push generated hex values to the array created at the begining of the function (prevColor).
+  prevColor.push(hexVal1);
+  prevColor.push(hexVal2);
+  // Changes the pages background color to the one previously generated.
+  body.style.background =
+    "linear-gradient(to right, " + hexVal1 + ", " + hexVal2 + ")";
+  // Add the rgb and hex values to the respective tags in the index.html file.
+  rbg.textContent = body.style.background + ";";
+  hex.textContent = prevColor;
 }
 
 // Adding an event listener to the input buttons in the webpage that "listen" for a input and when a input is selected the function gradient is run.
-color1.addEventListener("input", gradient); 
+color1.addEventListener("input", gradient);
 color2.addEventListener("input", gradient);
 // Adding an event listener to the button named "Random", which runs the function randomButton on a click to the button.
 rndBttn.addEventListener("click", randomButton);
